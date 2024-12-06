fd7c949b2a63fbacc38858048481e1bb5da9f7e6
Brian Futrell <wfutrell70@twc.com>
2024-12-06 14:48:28 -0500

    fixed issue preventing the alert form appearing - thanks Héctor!

--- MVVMDemo/MVVM/ViewModels/CommandsViewModel.cs
+++ MVVMDemo/MVVM/ViewModels/CommandsViewModel.cs
@@ -10,7 +10,7 @@ namespace MVVMDemo.MVVM.ViewModels
 {
     public class CommandsViewModel
     {
-        public ICommand ClickCommand() =>
+        public ICommand ClickCommand =>
             new Command(() => App.Current.MainPage.DisplayAlert("Title", "message", "Ok"));
 
         //public ICommand ClickCommand()
